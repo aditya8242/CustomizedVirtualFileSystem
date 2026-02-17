e17633c89ebd46478e28821589f1291720906213
aditya8242 <aditya.h.chavan@gmail.com>
2026-02-17 13:19:01 +0530

    added headers

---
+++ cvfs.h
@@ -0,0 +1,7 @@
+// required header files
+
+#include<stdio.h>	// basic I/O
+#include<stdlib.h>	// malloc()
+#include<unistd.h>	// file I/O
+#include<stdbool.h>	// boolean
+#include<string.h>	// string functions
